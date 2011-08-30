d8b0f4af13f4fed8ec44405a7ff44ff94c69de64
frostbyte <frostbyte.cpp@gmail.com>
2011-08-30 17:54:56 +0530

    Fixed project name conflict

--- urls.py
+++ urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'code_frost.views.home', name='home'),
-    # url(r'^code_frost/', include('code_frost.foo.urls')),
+    # url(r'^$', 'code-frost.views.home', name='home'),
+    # url(r'^code-frost/', include('code_frost.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
